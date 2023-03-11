f3c47b9ed96d5688a2d7f341ee2688c9faf26d09
Mark Anthony Ponce <markponce07@gmail.com>
2023-03-11 23:16:09 +0800

    Add ToastContainer and ReactToastify.css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
 import 'tailwindcss/tailwind.css'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />
+const App = ({ Component, pageProps }) => {
+    return (
+        <>
+            <Component {...pageProps} />
+            <ToastContainer />
+        </>
+    )
+}
 
 export default App
