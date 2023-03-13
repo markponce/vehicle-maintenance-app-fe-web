d174b2c5ca149a13181aad035e795bf377be1fff
Mark Anthony Ponce <markponce07@gmail.com>
2023-03-13 14:20:04 +0800

    Add modal and placeholder

--- src/pages/makes/create.js
+++ src/pages/makes/create.js
@@ -11,9 +11,11 @@ import ButtonLink from '@/components/ButtonLink'
 import Spinner from '@/components/Spinner'
 import { useStoreMake } from '@/hooks/make'
 import { toast } from 'react-toastify'
+import MyModal from '@/components/MyModal'
 
 const Create = () => {
     const router = useRouter()
+    const [isModalOpen, setIsModalOpen] = useState(false)
     const [name, setName] = useState('')
     const { newMake, error, trigger, isMutating, reset } = useStoreMake({
         throwOnError: false,
@@ -24,7 +26,7 @@ const Create = () => {
                 autoClose: 2000,
                 type: 'success',
             })
-            await router.replace(`/makes/${data.id}`)
+            await router.replace(`/makes/`)
         },
         onError: (err, key, config) => {
             if (typeof err == 'string') {
@@ -35,24 +37,20 @@ const Create = () => {
                 })
             }
         },
+        // revalidate: false,
+        // optimisticData: data => {
+        //     console.log({ data, newMake })
+        //     return data
+        // },
+        // populateCache: (updatedTodo, todos) => {
+        //     console.log({ updatedTodo, todos })
+        //     return [...todos, updatedTodo]
+        // },
     })
     return (
         <AppLayout
             header={
                 <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="inline-block h-6 w-6">
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M12 4.5v15m7.5-7.5h-15"
-                        />
-                    </svg>{' '}
                     New Make
                 </h2>
             }>
@@ -64,13 +62,44 @@ const Create = () => {
                     <div className="overflow-hidden bg-white shadow-sm  sm:rounded-lg">
                         <div className="border-b border-gray-200  bg-white p-6">
                             <div>
+                                <MyModal
+                                    isOpen={isModalOpen}
+                                    title={
+                                        <h3 className="text-center">
+                                            Create Make
+                                        </h3>
+                                    }
+                                    message={
+                                        <p className="text-center">
+                                            Are your sure?
+                                        </p>
+                                    }
+                                    // onClose={() => alert(123)}
+                                    buttons={
+                                        <div className="flex justify-between">
+                                            <Button
+                                                onClick={() =>
+                                                    setIsModalOpen(false)
+                                                }>
+                                                No
+                                            </Button>
+                                            <Button
+                                                onClick={() => {
+                                                    trigger({ name })
+                                                    setIsModalOpen(false)
+                                                }}>
+                                                Yes
+                                            </Button>
+                                        </div>
+                                    }
+                                />
                                 {/* <div>error: {JSON.stringify(error)}</div>
                                 <div>data: {JSON.stringify(make)}</div> */}
                                 <form
                                     className="mb-4 space-y-4"
                                     onSubmit={e => {
                                         e.preventDefault()
-                                        trigger({ name })
+                                        setIsModalOpen(true)
                                     }}>
                                     <div className="flex justify-between">
                                         <ButtonLink
@@ -127,6 +156,9 @@ const Create = () => {
 
                                         <div className="">
                                             <Input
+                                                placeHolder={
+                                                    'E.g. Yamaha, Honda, Kawasaki, etc...'
+                                                }
                                                 disabled={isMutating}
                                                 id="name"
                                                 type="text"
