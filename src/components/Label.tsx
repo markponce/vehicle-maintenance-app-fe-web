f6706bacf6429a19a6006620dc90e252cb01f821
Mark Anthony Ponce <markponce07@gmail.com>
2023-03-26 11:23:25 +0800

    fix ts

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -4,7 +4,7 @@ const Label = ({
     className = '',
     children,
     ...props
-}: PropsWithChildren<React.HTMLAttributes<HTMLLabelElement>>) => (
+}: PropsWithChildren<React.LabelHTMLAttributes<HTMLLabelElement>>) => (
     <label
         className={`${className} block text-sm font-medium text-gray-700`}
         {...props}>
