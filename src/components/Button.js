8af9137a262a09b2761e5728e85e3a94cfdaa070
Mark Anthony Ponce <markponce07@gmail.com>
2023-03-26 10:29:36 +0800

    delete js files

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({ type = 'submit', className, ...props }) => (
-    <button
-        type={type}
-        className={`${className} inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white ring-gray-300 transition duration-150 ease-in-out hover:bg-gray-700 focus:border-gray-900 focus:outline-none focus:ring active:bg-gray-900 disabled:opacity-25`}
-        {...props}
-    />
-)
-
-export default Button
--- src/utility/delay.js
+++
@@ -1,4 +0,0 @@
-export default function delay(ms = 2000) {
-    // console.log('Kindly remember to remove `sleep`')
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
